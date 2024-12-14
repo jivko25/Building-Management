b612c581fe6042de2cfffc5aa27ee5d1f7e907d9
Georgi Markov <gogata1905@gmail.com>
2024-12-14 17:45:45 +0200

    replace config.json with data.config.js so we can use .env

--- server/data/config/database.js
+++ server/data/config/database.js
@@ -1,20 +1,22 @@
-const { Sequelize } = require('sequelize');
-const config = require('./config.json')[process.env.NODE_ENV || 'development'];
+const { Sequelize } = require("sequelize");
+const config = require("./config/database.js")[
+  process.env.NODE_ENV || "development"
+];
 
 const sequelize = new Sequelize(
-    config.database,
-    config.username,
-    config.password,
-    {
-        host: config.host,
-        dialect: config.dialect,
-        pool: {
-            max: 5,
-            min: 0,
-            acquire: 30000,
-            idle: 10000
-        }
-    }
+  config.database,
+  config.username,
+  config.password,
+  {
+    host: config.host,
+    dialect: config.dialect,
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
+  }
 );
 
 module.exports = sequelize;
