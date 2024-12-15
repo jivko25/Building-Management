10b787f980b326201403cae225f4d7065a37fd33
Georgi Markov <gogata1905@gmail.com>
2024-12-15 16:58:20 +0200

    add normal seeded user

--- server/data/seeders/20241204130244-demo-users.js
+++ server/data/seeders/20241204130244-demo-users.js
@@ -8,12 +8,14 @@ module.exports = {
     const saltRounds = 10;
     const hashedAdminPassword = await bcrypt.hash("admin123", saltRounds);
     const hashedManagerPassword = await bcrypt.hash("manager123", saltRounds);
+    const hashedUserPassword = await bcrypt.hash("user123", saltRounds);
 
     await queryInterface.bulkInsert(
       "tbl_users",
       [
         { id: 1, full_name: "Админ Админов", username: "admin", hashedPassword: hashedAdminPassword, role: "admin", status: "active", manager_id: null },
-        { id: 2, full_name: "Мениджър Мениджъров", username: "manager", hashedPassword: hashedManagerPassword, role: "manager", status: "active", manager_id: 1 }
+        { id: 2, full_name: "Мениджър Мениджъров", username: "manager", hashedPassword: hashedManagerPassword, role: "manager", status: "active", manager_id: 1 },
+        { id: 3, full_name: "Потребител Потребителов", username: "user", hashedPassword: hashedUserPassword, role: "user", status: "active", manager_id: 2 }
       ],
       {}
     );
