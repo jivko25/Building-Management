0bba9f71b0b3f8cb8b46ecf4e7077522a42c456d
Georgi Markov <gogata1905@gmail.com>
2024-12-16 16:49:54 +0200

    fix artisans edit

--- client/src/components/common/FormElements/FormCompanySelector.tsx
+++ client/src/components/common/FormElements/FormCompanySelector.tsx
@@ -1,74 +1,66 @@
 import {
-    FormControl,
-    FormField,
-    FormItem,
-    FormLabel,
-} from '@/components/ui/form';
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
 import {
-    Select,
-    SelectContent,
-    SelectItem,
-    SelectTrigger,
-    SelectValue,
-} from '@/components/ui/select';
-import { Company } from '@/types/company-types/companyTypes';
-import { TableFormSelectType } from '@/types/table-types/tableTypes';
-import { useFormContext } from 'react-hook-form';
-import { PaginatedData } from '../Pagination/Pagination';
-import { useFetchDataQuery } from '@/hooks/useQueryHook';
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Company } from "@/types/company-types/companyTypes";
+import { TableFormSelectType } from "@/types/table-types/tableTypes";
+import { useFormContext } from "react-hook-form";
+import { PaginatedData } from "../Pagination/Pagination";
+import { useFetchDataQuery } from "@/hooks/useQueryHook";
 
 const CompanySelector = ({
-    label,
-    name,
-    placeholder,
-    defaultVal,
+  label,
+  name,
+  placeholder,
+  defaultVal,
 }: TableFormSelectType) => {
-    const { control } = useFormContext();
+  const { control } = useFormContext();
 
-    const { data: companies } = useFetchDataQuery<PaginatedData<Company>>({
-        URL: '/companies',
-        queryKey: ['companies'],
-        options: {
-            staleTime: Infinity,
-        },
-    });
+  const { data: companies } = useFetchDataQuery<PaginatedData<Company>>({
+    URL: "/companies",
+    queryKey: ["companies"],
+    options: {
+      staleTime: Infinity,
+    },
+  });
 
-    return (
-        <FormField
-            control={control}
-            name={name}
-            render={({ field }) => (
-                <FormItem>
-                    <FormLabel className='font-semibold'>{label}</FormLabel>
-                    <Select
-                        onValueChange={field.onChange}
-                        defaultValue={defaultVal}
-                    >
-                        <FormControl>
-                            <SelectTrigger>
-                                <SelectValue placeholder={placeholder} />
-                            </SelectTrigger>
-                        </FormControl>
-                        <SelectContent>
-                            {companies &&
-                                companies.data
-                                    .filter(
-                                        (company) => company.status === 'active'
-                                    )
-                                    .map((company) => (
-                                        <SelectItem
-                                            key={company.id}
-                                            value={company.name}
-                                        >
-                                            {company.name}
-                                        </SelectItem>
-                                    ))}
-                        </SelectContent>
-                    </Select>
-                </FormItem>
-            )}
-        />
-    );
+  return (
+    <FormField
+      control={control}
+      name={name}
+      render={({ field }) => (
+        <FormItem>
+          <FormLabel className="font-semibold">{label}</FormLabel>
+          <Select onValueChange={field.onChange} defaultValue={defaultVal}>
+            <FormControl>
+              <SelectTrigger>
+                <SelectValue placeholder={placeholder} />
+              </SelectTrigger>
+            </FormControl>
+            <SelectContent>
+              {companies &&
+                companies?.data
+                  ?.filter((company) => company.status === "active")
+                  .map((company) => (
+                    <SelectItem key={company.id} value={company.name}>
+                      {company.name}
+                    </SelectItem>
+                  ))}
+            </SelectContent>
+          </Select>
+        </FormItem>
+      )}
+    />
+  );
 };
 
 export default CompanySelector;
