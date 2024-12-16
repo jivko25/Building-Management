710f95774a1e04ff12c7aac59126be01ae7c1c34
Georgi Markov <gogata1905@gmail.com>
2024-12-16 17:25:35 +0200

    fix Select user during create new artisan

--- client/src/types/query-data-types/paginatedDataTypes.ts
+++ client/src/types/query-data-types/paginatedDataTypes.ts
@@ -1,44 +1,44 @@
-import { QueryKey, UseQueryOptions } from '@tanstack/react-query';
-import { PaginatedWorkItems } from '../work-item-types/workItem';
-import { ProjectTask } from '../task-types/taskTypes';
+import { QueryKey, UseQueryOptions } from "@tanstack/react-query";
+import { PaginatedWorkItems } from "../work-item-types/workItem";
+import { ProjectTask } from "../task-types/taskTypes";
 
 export type UseFetchQueryOptions<TData> = Omit<
-    UseQueryOptions<TData>,
-    'queryKey' | 'queryFn'
+  UseQueryOptions<TData>,
+  "queryKey" | "queryFn"
 >;
 
 export type CachedDataOptions<TData> = {
-    queryKey: QueryKey;
-    selectFn: (
-        data:
-            | PaginatedDataResponse<TData>
-            | TData[]
-            | PaginatedWorkItems
-            | ProjectTask
-    ) => TData | undefined;
+  queryKey: QueryKey;
+  selectFn: (
+    data:
+      | PaginatedDataResponse<TData>
+      | TData[]
+      | PaginatedWorkItems
+      | ProjectTask
+  ) => TData | undefined;
 };
 
 export interface FetchQueryOptions {
-    URL: string;
-    queryKey: QueryKey;
+  URL: string;
+  queryKey: QueryKey;
 }
 
 export interface FetchDataQueryOptions<TData> extends FetchQueryOptions {
-    options?: UseFetchQueryOptions<TData>;
+  options?: UseFetchQueryOptions<TData>;
 }
 
 export interface UseGetPaginatedDataTypes extends FetchQueryOptions {
-    page: number;
-    limit?: number;
-    search?: string;
+  page: number;
+  limit?: number;
+  search?: string;
 }
 
 export interface PaginatedDataResponse<T> {
-    users?: T[];
-    data?: T[];
-    usersCount?: number;
-    totalCount?: number;
-    page: number;
-    limit: number;
-    totalPages: number;
+  users?: T[];
+  data?: T[];
+  usersCount?: number;
+  totalCount?: number;
+  page: number;
+  limit: number;
+  totalPages: number;
 }
