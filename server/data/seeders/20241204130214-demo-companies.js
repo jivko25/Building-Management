a7270743dde9aff3cce7bdae2de667555f5a9730
Georgi Markov <gogata1905@gmail.com>
2024-12-17 16:56:40 +0200

    fix seed companies

--- server/data/seeders/20241204130214-demo-companies.js
+++ server/data/seeders/20241204130214-demo-companies.js
@@ -6,8 +6,8 @@ module.exports = {
     await queryInterface.bulkInsert(
       "tbl_companies",
       [
-        { id: 1, name: "Строителна фирма А", number: "BG123456789", address: "ул. Примерна 1", mol: "Георги Георгиев", email: "contact@firma-a.com", phone: "0888123456", dds: "yes", status: "active" },
-        { id: 2, name: "Строителна фирма Б", number: "BG987654321", address: "ул. Примерна 2", mol: "Мария Маринова", email: "contact@firma-b.com", phone: "0888765432", dds: "no", status: "inactive" }
+        { id: 1, name: "Строителна фирма А", number: "BG123456789", address: "ул. Примерна 1", mol: "Георги Георгиев", email: "contact@firma-a.com", phone: "0888123456", dds: "yes", status: "active", creator_id: 1 },
+        { id: 2, name: "Строителна фирма Б", number: "BG987654321", address: "ул. Примерна 2", mol: "Мария Маринова", email: "contact@firma-b.com", phone: "0888765432", dds: "no", status: "inactive", creator_id: 1 }
       ],
       {}
     );
