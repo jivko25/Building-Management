8e933168120f9701b1ef71c96b27f08d8705fd6b
Georgi Markov <gogata1905@gmail.com>
2024-12-18 12:14:49 +0200

    Enhance editArtisanController to include activity_id and measure_id

--- server/controllers/artisans/editArtisanController.js
+++ server/controllers/artisans/editArtisanController.js
@@ -5,7 +5,7 @@ const ApiError = require("../../utils/apiError");
 
 const editArtisan = async (req, res, next) => {
   const artisanId = req.params.id;
-  const { name, note, number, email, company, artisanName, status } = req.body;
+  const { name, note, number, email, company, artisanName, status, activity_id, measure_id } = req.body;
 
   try {
     const artisan = await Artisan.findByPk(artisanId);
@@ -25,7 +25,9 @@ const editArtisan = async (req, res, next) => {
       email,
       companyId: companyRecord.id,
       user_id: userRecord.id,
-      status
+      status,
+      activity_id,
+      measure_id
     });
 
     res.json({
