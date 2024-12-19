c6caa45c29104b3a74c7dac7faba6d3e1b9212b7
jivko25 <jivkou@yahoo.com>
2024-12-20 00:28:49 +0200

    feature: add email service

---
+++ server/controllers/emailService/emailService.js
@@ -0,0 +1,32 @@
+const nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.NODEMAILER_EMAIL,
+    pass: process.env.NODEMAILER_PASSWORD
+  },
+  tls: {
+    rejectUnauthorized: false
+  }
+});
+
+const sendMail = async (req, res, next) => {
+  const mailOptions = {
+    from: process.env.NODEMAILER_EMAIL,
+    to: req.body.receiver,
+    subject: req.body.subject,
+    text: req.body.text
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return res.status(500).send(error.toString());
+    }
+    res.send("Email sent: " + info.response);
+  });
+};
+
+module.exports = {
+  sendMail
+};
