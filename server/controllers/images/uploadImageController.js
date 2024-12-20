b58682aa696b0b05868396963ba5c9cd701b4b2f
Михаел Йорданов <gaws@mail.bg>
2024-12-20 20:38:33 +0200

    add an endpoint to upload image

--- server/controllers/images/uploadImageController.js
+++ server/controllers/images/uploadImageController.js
@@ -1,9 +1,18 @@
+const s3Service = require("../../services/s3Service");
 const uploadImage = async (req, res) => {
-  console.log("from body", req.body);
-  console.log("from multer", req.file);
-  res.status(200).json({
-    message: "Image uploaded successfully!",
-    image: req.file
-  });
+  try {
+    const { response, imageUrl } = await s3Service.uploadImageAsync(req.file);
+    res.status(200).json({
+      message: "Image uploaded successfully!",
+      image: imageUrl
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Image upload failed!",
+      error: error
+    });
+  }
 };
+
 module.exports = { uploadImage };
