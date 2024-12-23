c1e358659aec5ef8dcd0186f535df7c2ffaf287c
Михаел Йорданов <gaws@mail.bg>
2024-12-23 22:44:24 +0200

    Add default price button

--- client/src/components/tables/ArtisansTable/ArtisansCard.tsx
+++ client/src/components/tables/ArtisansTable/ArtisansCard.tsx
@@ -1,5 +1,6 @@
 //client\src\components\tables\ArtisansTable\ArtisansCard.tsx
 import EditArtisan from "@/components/Forms/Artisans/ArtisanFormEdit/EditArtisan";
+import ArtisanAddValues from "@/components/Forms/Artisans/ArtisanTableAddDefaultVlues/ArtisanAddValues";
 import { TableCell, TableRow } from "@/components/ui/table";
 import { Artisan } from "@/types/artisan-types/artisanTypes";
 
@@ -14,6 +15,7 @@ const ArtisansCard = ({ artisans }: ArtisansCardProps) => {
         <TableRow key={artisan.id}>
           <TableCell className="font-semibold">{artisan.name}</TableCell>
           <TableCell className="text-end w-[200px]">
+            <ArtisanAddValues />
             <EditArtisan artisanId={artisan.id!} />
           </TableCell>
         </TableRow>
