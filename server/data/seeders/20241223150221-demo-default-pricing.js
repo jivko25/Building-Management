d2616f08ca8b87263a67f2e41abd22b801e8687d
StiliyanIliev27 <stiliyaniliev2705@gmail.com>
2024-12-23 18:25:03 +0200

    renamed the seeder with correct extension

---
+++ server/data/seeders/20241223150221-demo-default-pricing.js
@@ -0,0 +1,26 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert('tbl_default_pricing', [
+      {
+        id: 1,
+        activity_id: 68,
+        measure_id: 1,
+        artisan_id: 1,
+        price: 100.0
+      },
+      {
+        id: 2,
+        activity_id: 72,
+        measure_id: 2,
+        artisan_id: 2,
+        price: 150.0
+      }
+    ], {});
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('tbl_default_pricing', null, {});
+  }
+};
