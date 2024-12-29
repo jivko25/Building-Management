75df0d14f188dc9b2e7c06926ec964e9e53eedb4
Михаел Йорданов <gaws@mail.bg>
2024-12-29 11:12:30 +0200

    Make changes, if default pricing exists to update it instead of throwing an error

--- server/controllers/defaultPricing/createDefaultPricingController.js
+++ server/controllers/defaultPricing/createDefaultPricingController.js
@@ -3,82 +3,88 @@ const { DefaultPricing, Artisan, Activity, Measure } = db;
 const ApiError = require("../../utils/apiError");
 
 const createDefaultPricing = async (req, res, next) => {
-    try {
-        const artisan_id = req.params.id;
+  try {
+    const artisan_id = req.params.id;
 
-        const isArtisan = await Artisan.findOne({
-            where: {
-                id: artisan_id
-            },
-            attributes: {
-                exclude: ["artisan_id"]
-            }
-        });
-        if (!isArtisan) {
-            throw new ApiError(404, "Artisan not found!");
-        }
-
-        const { activity_id, measure_id, price } = req.body;
-
-        if (!activity_id || !measure_id || !price || !artisan_id) {
-            throw new ApiError(400, "Activity id, measure id, price and artisan id are required!");
-        }
-
-        const isActivity = await Activity.findOne({
-            where: {
-                id: activity_id
-            },
-            attributes: {
-                exclude: ["activity_id"]
-            }
-        });
-        if (!isActivity) {
-            throw new ApiError(404, "Activity not found!");
-        }
-
-        const isMeasure = await Measure.findOne({
-            where: {
-                id: measure_id
-            },
-            attributes: {
-                exclude: ["measure_id"]
-            }
-        });
-        if (!isMeasure) {
-            throw new ApiError(404, "Measure not found!");
-        }
-
-        const isDefaultPricing = await DefaultPricing.findOne({
-            where: {
-                artisan_id: artisan_id,
-                activity_id: activity_id,
-                measure_id: measure_id
-            }
-        });
-        if (isDefaultPricing) {
-            throw new ApiError(400, "Default pricing already exists!");
-        }
-
-        const defaultPricing = await DefaultPricing.create({
-            activity_id,
-            measure_id,
-            artisan_id: artisan_id,
-            price
-        }); 
-
-        res.status(201).json({
-            message: "Default pricing created successfully!",
-            defaultPricing
-        });
-    } catch (error) {
-        if (error instanceof ApiError) {
-            next(error);
-        } else {
-            next(new ApiError(500, "Internal server Error!"));
-        }
+    const isArtisan = await Artisan.findOne({
+      where: {
+        id: artisan_id
+      },
+      attributes: {
+        exclude: ["artisan_id"]
+      }
+    });
+    if (!isArtisan) {
+      throw new ApiError(404, "Artisan not found!");
     }
+
+    const { activity_id, measure_id, price } = req.body;
+
+    if (!activity_id || !measure_id || !price || !artisan_id) {
+      throw new ApiError(400, "Activity id, measure id, price and artisan id are required!");
+    }
+
+    const isActivity = await Activity.findOne({
+      where: {
+        id: activity_id
+      },
+      attributes: {
+        exclude: ["activity_id"]
+      }
+    });
+    if (!isActivity) {
+      throw new ApiError(404, "Activity not found!");
+    }
+
+    const isMeasure = await Measure.findOne({
+      where: {
+        id: measure_id
+      },
+      attributes: {
+        exclude: ["measure_id"]
+      }
+    });
+    if (!isMeasure) {
+      throw new ApiError(404, "Measure not found!");
+    }
+
+    const isDefaultPricing = await DefaultPricing.findOne({
+      where: {
+        artisan_id: artisan_id,
+        activity_id: activity_id,
+        measure_id: measure_id
+      }
+    });
+    if (isDefaultPricing) {
+      if (isDefaultPricing.price === price) return;
+      isDefaultPricing.price = price;
+      await isDefaultPricing.save();
+      return res.status(200).json({
+        message: "Default pricing updated successfully!",
+        defaultPricing: isDefaultPricing
+      });
+    }
+
+    const defaultPricing = await DefaultPricing.create({
+      activity_id,
+      measure_id,
+      artisan_id: artisan_id,
+      price
+    });
+
+    res.status(201).json({
+      message: "Default pricing created successfully!",
+      defaultPricing
+    });
+  } catch (error) {
+    if (error instanceof ApiError) {
+      next(error);
+    } else {
+      next(new ApiError(500, "Internal server Error!"));
+    }
+  }
 };
 
 module.exports = {
-    createDefaultPricing
+  createDefaultPricing
 };
