9f0bba60506e93ae116a30a74d75e37fdc79aaee
Михаел Йорданов <gaws@mail.bg>
2024-12-30 09:39:25 +0200

    add default pricing response

--- client/src/types/defaultPricingType/defaultPricingTypes.ts
+++ client/src/types/defaultPricingType/defaultPricingTypes.ts
@@ -1,6 +1,9 @@
 export type DefaultPricing = {
   activity_id: string;
   measure_id: string;
-  artisan_id: string;
   price: number;
+};
+export type DefaultPricingResponse = {
+  message: string;
+  defaultPricing: DefaultPricing[];
 };
