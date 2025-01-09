3feab7e70ca61bf26417923794860a230fbd60e3
Georgi Markov <gogata1905@gmail.com>
2025-01-09 15:21:30 +0200

    successfully translate managers page

--- client/src/components/tables/ManagersTable/ManagerHeader.tsx
+++ client/src/components/tables/ManagersTable/ManagerHeader.tsx
@@ -1,13 +1,16 @@
 import TableHeader, { HeaderItems } from "@/components/common/TableElements/TableHeader";
-
-const managerHeaders: HeaderItems[] = [
-  { key: "name", label: "Name", width: "w-20rem", align: "left" },
-  { key: "email", label: "Email", width: "w-20rem", align: "left" },
-  { key: "role", label: "Role", width: "w-10rem", align: "left" },
-  { key: "options", label: "Options", width: "w-12.5rem", align: "right" }
-];
+import { useTranslation } from "react-i18next";
 
 const ManagersHeader = () => {
+  const { t } = useTranslation();
+
+  const managerHeaders: HeaderItems[] = [
+    { key: "name", label: t("Name"), width: "w-20rem", align: "left" },
+    { key: "email", label: t("Email"), width: "w-20rem", align: "left" },
+    { key: "role", label: t("Role"), width: "w-10rem", align: "left" },
+    { key: "options", label: t("Options"), width: "w-12.5rem", align: "right" }
+  ];
+
   return <TableHeader headers={managerHeaders} />;
 };
 
