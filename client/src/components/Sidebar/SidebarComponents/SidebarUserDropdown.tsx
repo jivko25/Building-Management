57a7fa88812b67b5a08e36f7ff94b9eb25e7bec3
Georgi Markov <gogata1905@gmail.com>
2025-01-09 15:15:51 +0200

    successfully translate sidebar

--- client/src/components/Sidebar/SidebarComponents/SidebarUserDropdown.tsx
+++ client/src/components/Sidebar/SidebarComponents/SidebarUserDropdown.tsx
@@ -10,12 +10,18 @@ import { Switch } from "@/components/ui/switch";
 import { PopoverClose } from "@radix-ui/react-popover";
 import { useState } from "react";
 import { useAuth } from "@/context/AuthContext";
-
+import { useTranslation } from "react-i18next";
+import i18n from "@/i18n/config";
 const SidebarUserDropdown = () => {
   const { theme, setTheme } = useTheme();
   const [isChecked, setIsChecked] = useState<boolean>(false);
   const { logout } = useAuth();
   const navigate = useNavigate();
+  const { t } = useTranslation();
+
+  console.log("Current translation key 'signOut':", t("signOut"));
+  console.log("Current translation key 'settings':", t("settings"));
+  console.log("Current language:", i18n.language);
 
   const handleLogoutClick = async () => {
     await logout();
@@ -30,9 +36,9 @@ const SidebarUserDropdown = () => {
   };
 
   const handleItemClick = (label: string, href: string) => {
-    if (label === "Sign out") {
+    if (label === t("signOut")) {
       handleLogoutClick();
-    } else if (label === "Settings") {
+    } else if (label === t("settings")) {
       navigate(href);
     }
   };
@@ -42,7 +48,7 @@ const SidebarUserDropdown = () => {
       <>
         <div className="flex items-center py-2">
           {theme === "dark" ? <Moon className="h-[1.2rem] w-[1.9rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" /> : <Sun className="h-[1.2rem] w-[1.9rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />}
-          <span className="text-sm pl-1 pr-6">{theme === "dark" ? "Dark Mode" : "Light Mode"}</span>
+          <span className="text-sm pl-1 pr-6">{theme === "dark" ? t("darkMode") : t("lightMode")}</span>
           <PopoverClose asChild>
             <Switch checked={isChecked} onCheckedChange={setIsChecked} onClick={themeToggleHandler} id="dark-mode" />
           </PopoverClose>
@@ -51,8 +57,8 @@ const SidebarUserDropdown = () => {
         {sidebarUserItems.links.map((link, index) => (
           <PopoverClose key={index} asChild>
             <Link to={link.href}>
-              <SidebarButton className="w-full p-1.5 text-sm" size="sm" icon={link.icon} onClick={() => handleItemClick(link.label, link.href)}>
-                {link.label}
+              <SidebarButton className="w-full p-1.5 text-sm" size="sm" icon={link.icon} onClick={() => handleItemClick(t(link.label), link.href)}>
+                {t(link.label)}
               </SidebarButton>
             </Link>
           </PopoverClose>
