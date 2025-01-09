5d89194fd6bbf597f5b208616fdfc88904ed8297
Georgi Markov <gogata1905@gmail.com>
2025-01-09 16:15:25 +0200

    successfully translate measures page

--- client/src/components/tables/MeasuresTable/MeasuresHeader.tsx
+++ client/src/components/tables/MeasuresTable/MeasuresHeader.tsx
@@ -1,12 +1,15 @@
 // src/components/tables/MeasuresTable/MeasuresHeader.tsx
 import TableHeader, { HeaderItems } from "@/components/common/TableElements/TableHeader";
-
-const measuresHeaders: HeaderItems[] = [
-  { key: "measure", label: "Measure", width: "w-20rem", align: "left" },
-  { key: "options", label: "Options", width: "w-12.5rem", align: "right" }
-];
+import { useTranslation } from "react-i18next";
 
 const MeasuresHeader = () => {
+  const { t } = useTranslation();
+
+  const measuresHeaders: HeaderItems[] = [
+    { key: "measure", label: t("Measure"), width: "w-20rem", align: "left" },
+    { key: "options", label: t("Options"), width: "w-12.5rem", align: "right" }
+  ];
+
   return <TableHeader headers={measuresHeaders} />;
 };
 
--- client/src/components/tables/MeasuresTable/MeasuresTableBody.tsx
+++ client/src/components/tables/MeasuresTable/MeasuresTableBody.tsx
@@ -21,12 +21,14 @@ import { Measure } from "@/types/measure-types/measureTypes";
 import MeasuresLoader from "@/utils/SkeletonLoader/Measures/MeasuresLoader";
 import MeasuresHeader from "./MeasuresHeader";
 import { CircleAlert } from "lucide-react";
+import { useTranslation } from "react-i18next";
 
 interface MeasureResponse {
   success: boolean;
   data: Measure[];
 }
 const MeasuresTableBody = () => {
+  const { t } = useTranslation();
   const {
     data: measuresResponse,
     isPending,
@@ -58,8 +60,8 @@ const MeasuresTableBody = () => {
             data={measuresResponse?.data || []}
             renderData={data => <MeasuresCard measures={data as Measure[]} />}
             noResults={{
-              title: "No measures found",
-              description: "It looks like you haven't added any measures yet.",
+              title: t("No measures found"),
+              description: t("It looks like you haven't added any measures yet."),
               Icon: Ruler
             }}
             wrapper={content => (
