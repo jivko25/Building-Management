fe9ad2da65cc90db573b8b07bac5d23986bed54e
jivko25 <jivkou@yahoo.com>
2025-01-13 09:49:44 +0200

    feature: add gallery

--- client/src/components/common/Breadcrumbs/TasksBreadcrumb.tsx
+++ client/src/components/common/Breadcrumbs/TasksBreadcrumb.tsx
@@ -1,10 +1,38 @@
-//client\src\components\common\Breadcrumbs\TasksBreadcrumb.tsx
-import CreateTask from "@/components/Forms/Tasks/TaskFormCreate/CreateTask";
+import React, { useRef } from "react";
 import { useParams } from "react-router-dom";
 import Breadcrumb from "./Breadcrumb";
+import apiClient from "@/api/axiosConfig";
+import CreateTask from "@/components/Forms/Tasks/TaskFormCreate/CreateTask";
+import { Button } from "@/components/ui/button";
 
 const TasksBreadcrumbs = () => {
   const { id } = useParams();
+  const fileInputRef = useRef<HTMLInputElement>(null);
+
+  const handleButtonClick = () => {
+    if (fileInputRef.current) {
+      fileInputRef.current.click(); // Отваря файловата система
+    }
+  };
+
+  const handleFileChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files ? event.target.files[0] : null;
+    if (!file) return;
+
+    const formData = new FormData();
+    formData.append("image", file);
+
+    try {
+      const response = await apiClient.post(`/images/upload/${id}`, formData, {
+        headers: {
+          "Content-Type": "multipart/form-data"
+        }
+      });
+      console.log("Image uploaded successfully:", response.data);
+    } catch (error) {
+      console.error("Error uploading image:", error);
+    }
+  };
 
   return (
     <div className="fixed top-0 left-0 md:left-60 right-0 z-40 pt-5 mt-14 bg-transparent backdrop-blur-sm">
@@ -22,8 +50,13 @@ const TasksBreadcrumbs = () => {
           ]}
         />
       </div>
-      <div className="flex flex-col border rounded-lg mx-8 space-y-4 p-4 backdrop-blur-sm bg-slate-900/20">
+      <div className="flex flex-row items-center border rounded-lg mx-8 p-4 backdrop-blur-sm bg-slate-900/20 gap-3">
         <CreateTask />
+        <Button variant="outline" onClick={handleButtonClick}>
+          <i className="pi pi-upload mr-3" style={{ color: "white", fontSize: "12px" }}></i>
+          Upload Image
+        </Button>
+        <input type="file" ref={fileInputRef} onChange={handleFileChange} className="hidden" />
       </div>
     </div>
   );
