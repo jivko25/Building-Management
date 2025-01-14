b5f10a934c86389a9db24a321ed63760b1a5086c
Georgi Markov <gogata1905@gmail.com>
2025-01-14 21:53:07 +0200

    add PR "Extended default pricing table #20"

--- server/data/seeders/20241223150221-demo-default-pricing.js
+++ server/data/seeders/20241223150221-demo-default-pricing.js
@@ -1,26 +1,32 @@
-'use strict';
-
+"use strict";
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkInsert('tbl_default_pricing', [
-      {
-        id: 1,
-        activity_id: 68,
-        measure_id: 1,
-        artisan_id: 1,
-        price: 100.0
-      },
-      {
-        id: 2,
-        activity_id: 72,
-        measure_id: 2,
-        artisan_id: 2,
-        price: 150.0
-      }
-    ], {});
+    await queryInterface.bulkInsert(
+      "tbl_default_pricing",
+      [
+        {
+          id: 1,
+          activity_id: 68,
+          measure_id: 1,
+          artisan_id: 1,
+          manager_id: 1,
+          project_id: 1,
+          price: 100.0
+        },
+        {
+          id: 2,
+          activity_id: 72,
+          measure_id: 2,
+          artisan_id: 2,
+          manager_id: 2,
+          project_id: 2,
+          price: 150.0
+        }
+      ],
+      {}
+    );
   },
-
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkDelete('tbl_default_pricing', null, {});
+    await queryInterface.bulkDelete("tbl_default_pricing", null, {});
   }
 };
