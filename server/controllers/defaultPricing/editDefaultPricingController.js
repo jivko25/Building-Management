37ee4d1ee8eaee0de642bebc21cf7e1056069b66
StiliyanIliev27 <stiliyaniliev2705@gmail.com>
2025-01-15 11:05:00 +0200

    Configured already existing endpoints with the new columns (creator_id, project_id, artisan_price and manager_price)

--- server/controllers/defaultPricing/editDefaultPricingController.js
+++ server/controllers/defaultPricing/editDefaultPricingController.js
@@ -1,14 +1,14 @@
 const db = require("../../data/index.js");
-const { DefaultPricing, Measure, Activity } = db;
+const { DefaultPricing, Measure, Activity, Project } = db;
 const ApiError = require("../../utils/apiError");
 
 const editDefaultPricing = async (req, res, next) => {
     try {
         const defaultPricingId = req.params.id;  
 
-        const { price, activity_id, measure_id } = req.body;
-        if (!price || !activity_id || !measure_id) {
-            throw new ApiError(400, "Price, activity id and measure id are required!");
+        const { artisan_price, manager_price, activity_id, measure_id, project_id } = req.body;
+        if (!artisan_price || !manager_price || !activity_id || !measure_id || !project_id) {
+            throw new ApiError(400, "Artisan price, manager price, activity id, measure id and project id are required!");
         }
 
         const isMeasure = await Measure.findByPk(measure_id, {
@@ -29,12 +29,21 @@ const editDefaultPricing = async (req, res, next) => {
             throw new ApiError(404, "Activity not found!");
         }
 
+        const isProject = await Project.findByPk(project_id, {
+            attributes: {
+                exclude: ["project_id"]
+            }
+        });
+        if (!isProject) {
+            throw new ApiError(404, "Project not found!");
+        }
+
         const defaultPricing = await DefaultPricing.findByPk(defaultPricingId);
         if (!defaultPricing) {
             throw new ApiError(404, "Default pricing not found!");
         }
 
-        await defaultPricing.update({ price, activity_id, measure_id });
+        await defaultPricing.update({ artisan_price, manager_price, activity_id, measure_id, project_id });
         res.status(200).json({ message: "Default pricing updated successfully!", defaultPricing });
     } catch (error) {
         if (error instanceof ApiError) {
