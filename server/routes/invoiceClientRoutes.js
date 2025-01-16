5d81424e115117af3dac7f1de87b2c9c2808832b
Georgi Markov <gogata1905@gmail.com>
2025-01-16 02:31:56 +0200

    add edit

--- server/routes/invoiceClientRoutes.js
+++ server/routes/invoiceClientRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const { createClientInvoice } = require("../controllers/invoiceController/CreateClientInvoice");
+const { editClientInvoice } = require("../controllers/invoiceController/EditClientInvoice");
 
 // Създаване на клиентска фактура
 router.post("/create", createClientInvoice);
-
+router.put("/:id/edit", editClientInvoice);
 
 module.exports = router;
