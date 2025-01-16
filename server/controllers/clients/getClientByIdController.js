513ce1bcca22f4a4aa661453e832d2d62f57b6ea
StiliyanIliev27 <stiliyaniliev2705@gmail.com>
2025-01-16 23:06:07 +0200

    Updated all endpoints which return not found 404 in api error to return empty array for FE

--- server/controllers/clients/getClientByIdController.js
+++ server/controllers/clients/getClientByIdController.js
@@ -1,33 +1,24 @@
 const db = require("../../data/index.js");
 const { Client, User } = db;
-const ApiError = require("../../utils/apiError");
 
 const getClientById = async (req, res, next) => {
-  try {
     const client = await Client.findByPk(req.params.id, {
-      include: [
-        {
-          model: User,
-          as: "creator",
-          attributes: ["username"]
-        }
-      ]
+        include: [
+            {
+                model: User,
+                as: "creator",
+                attributes: ["username"]
+            }
+        ]
     });
 
     if (!client) {
-      throw new ApiError(404, "Client not found!");
+        return res.json([]);
     }
 
     res.json(client);
-  } catch (error) {
-    if (error instanceof ApiError) {
-      next(error);
-    } else {
-      next(new ApiError(500, "Internal server Error!"));
-    }
-  }
 };
 
 module.exports = {
-  getClientById
+    getClientById
 };
--- server/controllers/user/getManagersController.js
+++ server/controllers/user/getManagersController.js
@@ -4,19 +4,13 @@ const User = db.User;
 const ApiError = require("../../utils/apiError");
 
 const getManagers = async (req, res, next) => {
-  console.log("Starting getManagers function");
-
   const isAdmin = req.user.role === "admin";
-
-  console.log(req.user);
 
   if (!isAdmin) {
     return next(new ApiError(403, "You are not authorized to access this resource!"));
   }
 
   try {
-    console.log("Executing database query for managers");
-
     const managers = await User.findAll({
       where: {
         role: "manager"
@@ -24,15 +18,16 @@ const getManagers = async (req, res, next) => {
       attributes: ["id", "full_name", "username", "role", "status", "readonly", "email"]
     });
 
-    console.log("Found managers:", managers);
-
     res.status(200).json({
       success: true,
       data: managers
     });
   } catch (error) {
-    console.error("Error in getManagers:", error);
-    next(new ApiError(500, "Error fetching managers", error));
+    if (error instanceof ApiError) {
+      next(error);
+    } else {
+      next(new ApiError(500, "Error fetching managers", error));
+    }
   }
 };
 
