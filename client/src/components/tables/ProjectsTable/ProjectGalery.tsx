2646932536bc8414ee923a87b2c57eb4256f19d7
jivko25 <jivkou@yahoo.com>
2025-01-16 23:21:11 +0200

    feature: final touches

--- client/src/components/tables/ProjectsTable/ProjectGalery.tsx
+++ client/src/components/tables/ProjectsTable/ProjectGalery.tsx
@@ -1,34 +1,46 @@
-import React, { useEffect, useState } from 'react';
-import { Image } from 'primereact/image';
-import apiClient from '@/api/axiosConfig';
-import { useParams } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { Image } from "primereact/image";
+import apiClient from "@/api/axiosConfig";
+import { useParams } from "react-router-dom";
 
 export type ImageType = {
-    id: number;
-    projectId: number;
-    imageUrl: string;
-}
+  id: number;
+  projectId: number;
+  imageUrl: string;
+};
 
-interface ProjectGalleryProps {
-}
+interface ProjectGalleryProps {}
 
 const ProjectGallery: React.FC<ProjectGalleryProps> = () => {
-    const [images, setImages] = useState<ImageType[]>([]);
-    const { id } = useParams();
+  const [images, setImages] = useState<ImageType[]>([]);
+  const { id } = useParams();
 
-    useEffect(() => {
-        const fetchImages = async () => {
-          apiClient.get(`/images/${id}`).then(response => {
-            setImages(response.data.images);
-          });
-        };
-    
-        fetchImages();
-      }, [id]);
+  const handleDeleteImage = (id: number) => {
+    apiClient.delete(`/images/${id}`).then(response => {
+      setImages(images.filter(image => image.id !== id));
+    });
+    console.log(images)
+  }
+
+  useEffect(() => {
+    const fetchImages = async () => {
+      apiClient.get(`/images/${id}`).then(response => {
+        setImages(response.data.images);
+      });
+    };
+
+    fetchImages();
+  }, [id]);
   return (
-    <div className="card flex justify-content-center flex-wrap gap-4">
+    <div className="card flex justify-center flex-wrap gap-5">
       {images.map((item, index) => (
-        <Image key={index} src={item.imageUrl} alt={`Image ${index + 1}`} width="250" preview downloadable/>
+        <div key={index} className="relative flex items-center justify-center">
+          <Image key={index} src={item.imageUrl} alt={`Image ${index + 1}`} width="250" preview downloadable />
+          <i 
+          className="pi pi-trash absolute top-2 right-2 text-white rounded-full p-2 cursor-pointer" 
+          style={{ color: 'red' }} 
+          onClick={() => handleDeleteImage(item.id)}></i>
+        </div>
       ))}
     </div>
   );
