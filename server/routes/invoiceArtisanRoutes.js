3a21c27aa461a587c280ac24be597fd4c4d2f5bf
Georgi Markov <gogata1905@gmail.com>
2025-01-17 03:14:17 +0200

    fix get all artisan invoices

--- server/routes/invoiceArtisanRoutes.js
+++ server/routes/invoiceArtisanRoutes.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const { createArtisanInvoice } = require("../controllers/invoice/invoiceArtisan/CreateArtisanInvoiceController");
+const { getAllArtisanInvoices } = require("../controllers/invoice/invoiceArtisan/GetAllArtisanInvoicesController");
 
+// Artisan invoice routes
 router.post("/create", createArtisanInvoice);
+router.get("/", getAllArtisanInvoices);
 
 module.exports = router;
