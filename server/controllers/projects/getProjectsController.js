7befbdea45c8862f76bd67291d9ce60fea9ee7cb
jivko25 <jivkou@yahoo.com>
2025-01-17 22:25:19 +0200

    fix: getProjects

--- server/controllers/projects/getProjectsController.js
+++ server/controllers/projects/getProjectsController.js
@@ -4,41 +4,57 @@ const { Project, Artisan, TasksArtisan } = db;
 const { Op } = require("sequelize");
 
 const getProjects = async (req, res, next) => {
-    let whereClause = {};
-    const isAdmin = req.user.role === "admin";
+  let whereClause = {};
+  const isAdmin = req.user.role === "admin";
 
-    if (isAdmin) {
-        const projects = await Project.findAll();
-
-        if (projects.length === 0) {
-            return res.json([]);
-        }
-
-        return res.json(projects);
-    }
-
-    if (req.query.search) {
-        whereClause = {
-            ...whereClause,
-            [Op.or]: [{ name: { [Op.like]: `%${req.query.search}%` } }, { company_name: { [Op.like]: `%${req.query.search}%` } }]
-        };
-    }
-
-    whereClause.creator_id = req.user.id;
-
-    const projects = await Project.findAll({
-        attributes: ["id", "name", "company_id", "company_name", "email", "address", "location", "start_date", "end_date", "note", "status", "creator_id"],
-        where: whereClause,
-        order: [["id", "DESC"]]
-    });
+  if (isAdmin) {
+    const projects = await Project.findAll();
 
     if (projects.length === 0) {
-        return res.json([]);
+      return res.json([]);
     }
 
-    res.json(projects);
+    return res.json(projects);
+  }
+
+  if (req.user.role === "user") {
+    const artisan = await Artisan.findOne({ where: { user_id: req.user.id } });
+    if (artisan) {
+      const tasks = await TasksArtisan.findAll({ where: { artisan_id: artisan.id } });
+      if (tasks.length === 0) {
+        return res.json([]);
+      }
+      const taskIds = tasks.map(task => task.task_id);
+      const projects = await Project.findAll({ where: { id: { [Op.in]: taskIds } } });
+      if (projects.length === 0) {
+        return res.json([]);
+      }
+      return res.json(projects);
+    }
+  }
+
+  if (req.query.search) {
+    whereClause = {
+      ...whereClause,
+      [Op.or]: [{ name: { [Op.like]: `%${req.query.search}%` } }, { company_name: { [Op.like]: `%${req.query.search}%` } }]
+    };
+  }
+
+  whereClause.creator_id = req.user.id;
+
+  const projects = await Project.findAll({
+    attributes: ["id", "name", "company_id", "company_name", "email", "address", "location", "start_date", "end_date", "note", "status", "creator_id"],
+    where: whereClause,
+    order: [["id", "DESC"]]
+  });
+
+  if (projects.length === 0) {
+    return res.json([]);
+  }
+
+  res.json(projects);
 };
 
 module.exports = {
-    getProjects
+  getProjects
 };
