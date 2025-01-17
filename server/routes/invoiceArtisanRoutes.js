3acec463281aa62b4c30d8eeb020fed01daba16e
Georgi Markov <gogata1905@gmail.com>
2025-01-17 03:39:45 +0200

    change status

--- server/routes/invoiceArtisanRoutes.js
+++ server/routes/invoiceArtisanRoutes.js
@@ -3,10 +3,12 @@ const router = express.Router();
 const { createArtisanInvoice } = require("../controllers/invoice/invoiceArtisan/CreateArtisanInvoiceController");
 const { getAllArtisanInvoices } = require("../controllers/invoice/invoiceArtisan/GetAllArtisanInvoicesController");
 const { getArtisanInvoiceById } = require("../controllers/invoice/invoiceArtisan/GetArtisanInvoiceByIdController");
+const { updateArtisanInvoiceStatus } = require("../controllers/invoice/invoiceArtisan/UpdateArtisanInvoiceStatusController");
 
 // Artisan invoice routes
 router.post("/create", createArtisanInvoice);
 router.get("/", getAllArtisanInvoices);
 router.get("/:id", getArtisanInvoiceById);
+router.patch("/:id/status", updateArtisanInvoiceStatus);
 
 module.exports = router;
