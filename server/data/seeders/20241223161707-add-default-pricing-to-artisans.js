c269a412d04cdb638370977d4373da9f797cb1e0
Georgi Markov <gogata1905@gmail.com>
2025-01-20 17:41:02 +0200

    fix seed

--- server/data/seeders/20241223161707-add-default-pricing-to-artisans.js
+++ server/data/seeders/20241223161707-add-default-pricing-to-artisans.js
@@ -4,9 +4,15 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.bulkUpdate("tbl_artisans", { default_pricing_id: 1 }, { id: 1 });
     await queryInterface.bulkUpdate("tbl_artisans", { default_pricing_id: 2 }, { id: 2 });
+    await queryInterface.bulkUpdate("tbl_artisans", { default_pricing_id: 3 }, { id: 3 });
+    await queryInterface.bulkUpdate("tbl_artisans", { default_pricing_id: 4 }, { id: 4 });
+    await queryInterface.bulkUpdate("tbl_artisans", { default_pricing_id: 5 }, { id: 5 });
   },
   down: async (queryInterface, Sequelize) => {
     await queryInterface.bulkUpdate("tbl_artisans", { default_pricing_id: null }, { id: 1 });
     await queryInterface.bulkUpdate("tbl_artisans", { default_pricing_id: null }, { id: 2 });
+    await queryInterface.bulkUpdate("tbl_artisans", { default_pricing_id: null }, { id: 3 });
+    await queryInterface.bulkUpdate("tbl_artisans", { default_pricing_id: null }, { id: 4 });
+    await queryInterface.bulkUpdate("tbl_artisans", { default_pricing_id: null }, { id: 5 });
   }
 };
