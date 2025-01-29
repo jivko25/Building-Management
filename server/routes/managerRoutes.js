45dbcc6d1338d139bd71fb86f43e04f7bd871398
StiliyanIliev27 <stiliyaniliev2705@gmail.com>
2025-01-29 22:26:08 +0200

    added get manager reports functionality

---
+++ server/routes/managerRoutes.js
@@ -0,0 +1,10 @@
+//server\routes\managerRoutes.js
+const express = require("express");
+const authenticateToken = require("../middlewares/authenticateToken");
+const { getManagerReports } = require("../controllers/managers/getManagerReports");
+
+const router = express.Router();
+
+router.get("/managers/getReports/", authenticateToken, getManagerReports);
+
+module.exports = router;
