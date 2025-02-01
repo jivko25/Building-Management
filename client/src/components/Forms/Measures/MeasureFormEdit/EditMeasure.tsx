b4d91efd17cd506062bcecbe8f1458cadfbced83
jivko25 <jivkou@yahoo.com>
2025-02-01 15:53:09 +0200

    fix: edit measure

--- client/src/components/Forms/Measures/MeasureFormEdit/EditMeasure.tsx
+++ client/src/components/Forms/Measures/MeasureFormEdit/EditMeasure.tsx
@@ -9,6 +9,8 @@ import { useTranslation } from "react-i18next";
 import { useCachedData } from "@/hooks/useQueryHook";
 import { findItemById } from "@/utils/helpers/findItemById";
 import { Measure } from "@/types/measure-types/measureTypes";
+import apiClient from "@/api/axiosConfig";
+import { useQuery } from "@tanstack/react-query";
 
 type MeasureFormProps = {
   measureId: string;
@@ -19,21 +21,32 @@ const EditMeasure = ({ measureId }: MeasureFormProps) => {
   const { isOpen, setIsOpen } = useDialogState();
   const { useEditEntity } = useMutationHook();
 
-  const measure = useCachedData<Measure>({
-    queryKey: ["measures"],
-    selectFn: data => findItemById<Measure>(data as Measure[], measureId, measure => measure.id?.toString() || "")
+  // const measure = useCachedData<Measure>({
+  //   queryKey: ["measures"],
+  //   selectFn: data => findItemById<Measure>(data as Measure[], measureId, measure => measure.id?.toString() || "")
+  // });
+
+  const measure = useQuery<Measure>({
+    queryKey: ["measures", measureId],
+    queryFn: () => apiClient.get(`/measures/${measureId}`)
   });
+
+  console.log(measure?.data?.data?.data, 'measure');
+  
+
+
 
   const { mutate, isPending } = useEditEntity<MeasureSchema>({
     URL: `/measures/${measureId}/edit`,
     queryKey: ["measures"],
     successToast: t("Measure updated successfully!"),
     setIsOpen
+
   });
 
   const handleSubmit = useSubmitHandler(mutate, measureSchema);
 
-  return <DialogModal Component={EditMeasureForm} props={{ handleSubmit, isPending, measureId, initialData: measure }} isOpen={isOpen} setIsOpen={setIsOpen} title={t("Edit measure")} />;
+  return <DialogModal Component={EditMeasureForm} props={{ handleSubmit, isPending, measureId, initialData: measure?.data?.data?.data }} isOpen={isOpen} setIsOpen={setIsOpen} title={t("Edit measure")} />;
 };
 
 export default EditMeasure;
